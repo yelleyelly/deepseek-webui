d0b905631017e25bb4eeeb9719dc01f62353c81b
yelleyelly <163236263+yelleyelly@users.noreply.github.com>
2025-02-13 14:54:19 +0800

    Update config.ts

--- src/lib/api/config.ts
+++ src/lib/api/config.ts
@@ -1,11 +1,10 @@
 export const API_CONFIG = {
-  BASE_URL: process.env.NEXT_PUBLIC_DEEPSEEK_API_URL || 'https://api.deepseek.com/v1',
-  BASE_URL_V0: process.env.NEXT_PUBLIC_DEEPSEEK_API_URL_V0 || 'https://api.deepseek.com/v1',
+  BASE_URL: process.env.NEXT_PUBLIC_DEEPSEEK_API_URL || 'https://api.siliconflow.cn/v1',
+  BASE_URL_V0: process.env.NEXT_PUBLIC_DEEPSEEK_API_URL_V0 || 'https://api.siliconflow.cn/v1',
   BASE_COZE_URL: process.env.NEXT_PUBLIC_COZE_API_URL || 'https://api.coze.cn/v1',
   MODELS: {
-    'chat': 'deepseek-chat',
-    'coder': 'deepseek-coder',
-    'reasoner': 'deepseek-reasoner',
+    'chat': 'Pro/deepseek-ai/DeepSeek-V3',
+    'reasoner': 'Pro/deepseek-ai/DeepSeek-R1',
   },
 } as const;
 
